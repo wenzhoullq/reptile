a3c359e1974659ed24d722c4113c19d906ee113f
liqiang.li <liqiang.li@liqiang.li>
2023-05-09 14:34:03 +0800

    爬虫v1.0.3 支持csv文件读取

--- README.md
+++ README.md
@@ -2,18 +2,21 @@
 根据八爪鱼爬取的EXCEL进行下载
 excel里必须带有图片或则图片字段的文本
 ```shell
-windos环境
+#windos环境
 go build 
-.\reptile.exe C:/Users/54910/Desktop/sougou.xlsx 1
-linux环境
-先运行run.bat脚本,然后对文件授权
+.\reptile.exe C:/Users/54910/Desktop/sougou.xlsx 1 xlsx
+.\reptile.exe C:/Users/54910/Desktop/sougou.xlsx 1 csv
+xlsx代表slsx文件 csv代表csv文件
+#linux环境
+#先运行run.bat脚本,然后对文件授权
 chmod +x reptile
-.\reptile sougou.xlsx 0
+.\reptile sougou.xlsx 0 xlsx
+.\reptile sougou.xlsx 0 csv
+#xlsx代表slsx文件 csv代表csv文件
 ```
 第一个参数是xlsx的路径
 参数是文件路径,文件路径不要带空格
 第二个参数代表运行环境,如果是1代表是window上执行的,如果是0代表在linux上执行的
-windows环境下 xlsx文件路径需要 / 而不是 \ 进行隔离
 ### 功能说明
 
 最大并发数在MAXGET进行设置,一般不超过1000,http请求会开启新的协程,最后实际运行的协程是5倍
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"github.com/xuri/excelize/v2"
 	"io"
@@ -20,29 +21,70 @@ const (
 	MaxGet  = 400
 )
 
-var channel chan struct{} = make(chan struct{}, MaxGet)
+var channel = make(chan struct{}, MaxGet)
 
 func main() {
-	//cmd := exec.Command("reptile.exe")
-	//err := cmd.Run()
-	//if err != nil {
-	//	log.Println("启动失败:", err)
-	//} else {
-	//	log.Println("启动成功!")
-	//}
-	go func() {
-		log.Println(http.ListenAndServe(":6060", nil))
-	}()
-	excelFilePath := os.Args[1]
-	excelFileName := "Sheet1"
+	FilePath := os.Args[1]
 	savePath, _ := os.Getwd()
-	var wg sync.WaitGroup
 	var split = "/"
 	// 如果第二个启动参数是 1 则代表是在window环境运行 其它参数则是在linux上运行
 	if os.Args[2] == "1" {
 		split = "\\"
 	}
-	f, err := excelize.OpenFile(excelFilePath)
+	switch os.Args[3] {
+	case "xlsx":
+		downloadByEXCEL(FilePath, split, savePath)
+	case "csv":
+		downloadByCSV(FilePath, split, savePath)
+	default:
+		log.Println("启动参数错误,格式为 文件地址 系统环境(1是window环境,其余参数是linux环境) 文件格式(excel,csv)")
+	}
+}
+
+func downloadPic(wg *sync.WaitGroup, imgUrl, savePath, fileName, split string, num int) {
+	defer func() {
+		wg.Done()
+		channel <- struct{}{}
+	}()
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	res, err := client.Get(imgUrl)
+	if err != nil {
+		log.Println("A error occurred!", err)
+		return
+	}
+	defer res.Body.Close()
+	// 获得get请求响应的reader对象
+	reader := bufio.NewReaderSize(res.Body, 32*1024)
+	fileName = fmt.Sprintf("%s%d.jpg", fileName, num)
+	file, err := os.Create(savePath + split + fileName)
+	if err != nil {
+		panic(err)
+	}
+	// 获得文件的writer对象
+	writer := bufio.NewWriter(file)
+	_, _ = io.Copy(writer, reader)
+}
+
+func downloadByCSV(FilePath, split, savePath string) {
+	f, err := os.Open(FilePath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	csvReader := csv.NewReader(f)
+	rows, err := csvReader.ReadAll()
+	//读取的文件的文件名,如 "C:/Users/54910/Desktop/搜狗图片搜索 - 泥地.csv" ,读取的是 搜狗图片搜索 - 泥地.csv
+	strs := strings.Split(FilePath, split)
+	str := strs[len(strs)-1]
+	download(savePath, split, str, rows)
+
+}
+
+func downloadByEXCEL(FilePath, split, savePath string) {
+	f, err := excelize.OpenFile(FilePath)
 	if err != nil {
 		log.Println(err)
 		return
@@ -51,18 +93,24 @@ func main() {
 		if err := f.Close(); err != nil {
 		}
 	}()
+	excelFileName := "Sheet1"
 	rows, err := f.GetRows(excelFileName)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	strs := strings.Split(excelFilePath, "/")
+	strs := strings.Split(FilePath, split)
 	//读取的文件的文件名,如 "C:/Users/54910/Desktop/搜狗图片搜索 - 泥地.xlsx" ,读取的是 搜狗图片搜索 - 泥地.xlsx
 	str := strs[len(strs)-1]
 	//去除后缀.xlsx,最后名称是 搜狗图片搜索 - 泥地
 	str = str[:len(str)-5]
+	download(savePath, split, str, rows)
+
+}
+func download(savePath, split, str string, rows [][]string) {
+	wg := sync.WaitGroup{}
 	savePath = savePath + split + str
-	_, err = os.Stat(savePath)
+	_, err := os.Stat(savePath)
 	if os.IsNotExist(err) {
 		err = os.MkdirAll(savePath, os.ModePerm)
 		if err != nil {
@@ -84,12 +132,12 @@ func main() {
 		channel <- struct{}{}
 	}
 	var subSavePath string
+	cnt := 0
 	for i := 1; i < len(rows); i++ {
 		row := rows[i]
 		//每5000个创建一个文件夹
 		if i%FileMax == 1 {
 			subSavePath = savePath + split + time.Now().Format("20060102150405") + "_file" + strconv.Itoa(i/FileMax)
-			fmt.Println(subSavePath)
 			err = os.Mkdir(subSavePath, os.ModePerm)
 			if err != nil {
 				log.Println(subSavePath, "文件夹已存在", err)
@@ -102,36 +150,9 @@ func main() {
 		//wg 并发执行
 		wg.Add(1)
 		<-channel
-		go downloadPic(&wg, row[index], subSavePath, str, i%FileMax)
+		cnt++
+		go downloadPic(&wg, row[index], subSavePath, str, split, i%FileMax)
 	}
 	wg.Wait()
-	log.Println("程序结束:", time.Now())
-}
-
-func downloadPic(wg *sync.WaitGroup, imgUrl, savePath, fileName string, num int) {
-	defer func() {
-		wg.Done()
-		channel <- struct{}{}
-	}()
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-	res, err := client.Get(imgUrl)
-	if err != nil {
-		log.Println("A error occurred!", err)
-		return
-	}
-	defer res.Body.Close()
-	// 获得get请求响应的reader对象
-	reader := bufio.NewReaderSize(res.Body, 32*1024)
-	fileName = fmt.Sprintf("%s%d.jpg", fileName, num)
-	file, err := os.Create(savePath + "/" + fileName)
-	if err != nil {
-		panic(err)
-	}
-	// 获得文件的writer对象
-	writer := bufio.NewWriter(file)
-	_, _ = io.Copy(writer, reader)
-	//written, _ := io.Copy(writer, reader)
-	//fmt.Printf("Total length: %d", written)
+	log.Println("程序结束:", time.Now(), " 共下载图片", cnt, "张")
 }
